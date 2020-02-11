d66078e291cb1ef106c31b14db54e76b1aee6fbc
snowynam <snowynam1@gmail.com>
2020-02-11 16:03:30 +0900

    #crate a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ): string => CryptJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(index: number, hash: string, previousHash: string, data: string, timestamp:number){
         this.index = index;
         this.hash = hash;
@@ -16,6 +26,12 @@ class Block {
 const genesisBlock: Block = new Block(0,"2020202020", "", "hello", 123456);
 
 let blcokchain:[Block] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blcokchain;
+
+const getLastestBlock = (): Block => blcokchain[blcokchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blcokchain);
 
